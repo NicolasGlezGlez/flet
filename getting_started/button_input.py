c0c482e15ade1210b14b2ed9423731d96029dd1e
Nico <nicolas.gonzalez.gonzalez9633@gmail.com>
2023-09-24 16:45:12 +0200

    Move message from console to app (label)

--- getting_started/button_input.py
+++ getting_started/button_input.py
@@ -2,11 +2,16 @@ import flet as ft
 
 def main(page: ft.Page):
     txt_name = ft.TextField(label="Write you're name.")
+    lbl_salute = ft.Text()
+    
     def salute(e):
-        print (f"Hi, {txt_name.value}!")
+        lbl_salute.value = f"Hi, {txt_name.value}!"
+        page.update()
+        
     row = ft.Row(controls=[
         txt_name,
-        ft.ElevatedButton(text="Salute.", on_click=salute)
+        ft.ElevatedButton(text="Salute.", on_click=salute),
+        lbl_salute
     ])
     page.add(row)
     
